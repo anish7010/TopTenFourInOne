9ed0f100e77bad4102bbc6bead5c05c6853af2c8
Anish <anishkap7010@gmail.com>
2018-03-19 02:03:50 +0530

    Refining main menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { MoviesComponent } from './components/main-menu/movies/movies.component';
+import { MovieSearchComponent } from './components/main-menu/movies/movie-search/movie-search.component';
+import { MovieListComponent } from './components/main-menu/movies/movie-list/movie-list.component';
+import { MovieComponent } from './components/main-menu/movies/movie-list/movie/movie.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieSearchComponent,
+    MovieListComponent,
+    MovieComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
